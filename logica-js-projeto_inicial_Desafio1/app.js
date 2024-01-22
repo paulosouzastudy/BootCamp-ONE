c1a787fa10fd2c5f1f443091ebec66bb90d22d53
Paulo Cezar de Souza <paulosouza_study@outlook.com>
2024-01-22 14:38:56 -0300

    desafio 1

---
+++ logica-js-projeto_inicial_Desafio1/app.js
@@ -0,0 +1,13 @@
+alert ('Boas Vindas ao nosso site!');
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+alert ('Erro! Preencha todos os campos');
+let mensagemDeErro = ("Erro! Preencha todos os campos");
+alert(mensagemDeErro);
+let preenchaNome = prompt ('Preencha com seu nome');
+let preenchaIdade = prompt ('Digite sua idade');
+if (preenchaIdade>=18) {
+    alert ('Pode tirar a habilitação');
+}
